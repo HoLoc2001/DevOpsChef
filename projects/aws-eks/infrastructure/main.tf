7414648cbefe41ad5b683002baa57016ac587bdf
holoc2001 <hotanloc2001@gmail.com>
2026-06-18 21:15:24 +0700

    feat: initialize AWS EKS infrastructure with VPC and managed node groups

---
+++ projects/aws-eks/infrastructure/main.tf
@@ -0,0 +1,67 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc_name
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-southeast-1a", "ap-southeast-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = var.project_name
+  kubernetes_version = var.kubernetes_version
+
+  # Optional
+  endpoint_public_access = true
+
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+
+  compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
+  }
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+  }
+  addons = {
+    coredns = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      before_compute = true
+    }
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
---
+++ projects/aws-eks/infrastructure/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "ap-southeast-1"
+}
+
+variable "project_name" {
+  default = "my-eks-cluster"
+}
+
+variable "kubernetes_version" {
+  default = "1.36"
+}
+
+variable "vpc_name" {
+  default = "my-eks-vpc"
+}
